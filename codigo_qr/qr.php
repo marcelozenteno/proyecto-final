a1f6e6ceb7fb1f363fdbac95f7a5c5ef58afe0c2
marcelo <marcehugzen0202@gmail.com>
2020-11-04 08:50:08 -0400

    adicinando archivos para generar codigo qr

---
+++ codigo_qr/qr.php
@@ -0,0 +1,19 @@
+<?php 
+require 'phpqrcode/qrlib.php';
+
+$dir = 'temp/';
+
+if(!file_exists($dir))
+	mkdir($dir);
+
+$filename = $dir.'test.png';
+
+$tamanio = 10;
+$level = 'H';
+$frameSize = 5;
+$contenido = 'Registro Exitoso';
+
+QRcode::png($contenido, $filename, $level, $tamanio, $frameSize);
+
+echo '<img src="'.$filename.'"/>';
+ ?>
